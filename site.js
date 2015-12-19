123b06e52d65eb0d89213476aea790d71e41bcbe
Nikolay <NKatkov@yandex.ru>
2015-12-19 23:57:36 +0000

    Initial commit

---
+++ site.js
@@ -0,0 +1,35 @@
+var express = require('express'),
+    path = require('path'),
+    app = module.exports = express();
+    mongoStore = require('connect-mongodb'),
+    favicon = require(‘serve-favicon’),
+    Settings = { development: {}, test: {}, production: {} };
+
+
+
+var staticSiteOptions = {
+   portnum: 80, // слушать порт 80
+   maxAge: 1000 * 60 * 15 // хранить страницы в кэше пятнадцать минут
+};
+
+function mongoStoreConnectionArgs() {
+  return { dbname: "Web",
+           host: "127.0.0.1",
+           port: "27017",
+           username: "admin",
+           password: "do5ita" };
+}
+
+
+  app.set('views', __dirname + '/views');
+  app.use(express.favicon());
+  app.use(express.bodyDecoder());
+  app.use(express.cookieDecoder());
+  app.use(express.session({store: mongoStore(mongoStoreConnectionArgs())}));
+  app.use(express.logger({ format: '\x1b[1m:method\x1b[0m \x1b[33m:url\x1b[0m :response-time ms' }))
+  app.use(express.methodOverride());
+  app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
+  app.use(app.router);
+  app.use(express.staticProvider(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'static'),staticSiteOptions)).listen(staticSiteOptions.portnum);
+
