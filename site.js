234885e2b51404fa5e371ffdfe48167c6a2dc4c0
NKatkov <NKatkov@yandex.ru>
2015-12-23 18:32:24 +0300

    Update site.js

--- site.js
+++ site.js
@@ -24,7 +24,7 @@ function mongoStoreConnectionArgs() {
 
   app.set('views', __dirname + '/views');
   //app.use(favicon(options.favicon));
-  app.use(bodyParser());
+  app.use(express.bodyParser());
   app.use(express.cookie-parser());
   app.use(express.session({store: mongoStore(mongoStoreConnectionArgs())}));
   app.use(express.logger({ format: '\x1b[1m:method\x1b[0m \x1b[33m:url\x1b[0m :response-time ms' }))
